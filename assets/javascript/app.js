ca6da7f8479ab087886dd6dc4c6a66147946e594
Zaldana <alexandrazaldana@gmail.com>
2019-02-07 12:21:30 -0800

    added footer cleaned up portfolio page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,76 @@
 $(document).ready(function(){ 
 
+    var config = {
+        apiKey: "AIzaSyDQEhx6b8Rk_K6M7FwOlg7YXf7d2zi6s5c",
+        authDomain: "portfolio-b21ec.firebaseapp.com",
+        databaseURL: "https://portfolio-b21ec.firebaseio.com",
+        projectId: "portfolio-b21ec",
+        storageBucket: "portfolio-b21ec.appspot.com",
+        messagingSenderId: "946041206431"
+      };
+      firebase.initializeApp(config);
+
+    var database = firebase.database();
+
     var homePage = $(".home-page");
     var mainPage = $(".main-page");
     var mainText = $(".about-me");
     var portfolio = $(".portfolio");
     var contact = $(".contact");
     var leftSide = $(".left-side");
-    var graphicDesign = $(".gdesign");
-    var webDesign = $(".web");
-    var branding = $(".brand");
-    var layout = $(".layout-design");
-    var freelance = $(".free");
-    var portfolioHeader = $("#about");
+    var form = $('form');
+    var logo = $("#logo");
+
+    $("#submit").on('click', function(event){
+        event.preventDefault();
+
+        var first = $("#firstName").val().trim();
+        var last = $("#lastName").val().trim();
+        var emailAddress = $("#email").val().trim();
+        var subjectLine = $("#subject").val().trim();
+        var comment = $("#message").val().trim();
+        form.hide();
+        mainText.show();
+        $("#home").css('font-weight', 'bold');
+        $("#contact").css('font-weight', 'normal');
+
+        console.log(first, last, emailAddress, subjectLine, comment);
+
+        $("#firstName").val('');
+        $("#lastName").val('');
+        $("#emailName").val('');
+        $("#subject").val('');
+        $("#message").val('');
+
+        database.ref().push({
+            first:first,
+            last:last,
+            emailAddress:emailAddress,
+            subjectLine:subjectLine,
+            comment:comment,
+        })
+        return false;
+    })
+
+    database.ref().on("child_added", function(snap){
+        var save = snap.val();
+
+        console.log(snap.key, snap.val());
+    
+        console.log(save.first);
+        console.log(save.last);
+        console.log(save.emailAddress);
+        console.log(save.subjectLine);
+        console.log(save.comment);
+    })
 
     function viewPortfolio(){
         var homePageButton = $("#startButton");
         mainPage.hide();
         portfolio.hide();
         contact.hide();
-        layout.hide();
         leftSide.hide();
+        form.hide();
 
         homePageButton.on('click', function (){
             homePage.empty();
@@ -30,17 +81,54 @@ $(document).ready(function(){
 
     viewPortfolio();
 
+    function clickLogo(){
+        logo.on('click', function(){
+            homePage.empty();
+            mainText.show();
+            portfolio.hide();
+            contact.hide();
+            leftSide.hide();
+            form.hide();
+            $("#home").css('font-weight', 'bold');
+            $("#portfolio").css('font-weight', 'normal');
+            $("#contact").css('font-weight', 'normal');
+
+
+        })
+    }
+
+    clickLogo();
+
+    function clickHome(){
+        var homeButton = $("#home");
+
+        homeButton.on('click', function (){
+            homePage.empty();
+            mainText.show();
+            portfolio.hide();
+            contact.hide();
+            leftSide.hide();  
+            form.hide();
+            $(".graphic").css('font-weight', 'normal');
+            $("#home").css('font-weight', 'bold');
+            $("#portfolio").css('font-weight', 'normal');
+            $("#contact").css('font-weight', 'normal');
+        });
+    }
+
+    clickHome();
+
     function clickPortfolio(){
         var portfolioButton = $("#portfolio");
 
         portfolioButton.on('click', function (){
             homePage.empty();
-            contact.hide();
             mainText.hide();
-            layout.hide();
-            leftSide.show();
             portfolio.show();
-            $(".graphic").css('font-weight', 'bold');
+            contact.hide();
+            leftSide.show();
+            form.hide();
+            $(".graphic").css('font-weight', 'normal');
             $("#home").css('font-weight', 'normal');
             $("#portfolio").css('font-weight', 'bold');
             $("#contact").css('font-weight', 'normal');
@@ -53,34 +141,16 @@ $(document).ready(function(){
 
     clickPortfolio();
 
-    function clickHome(){
-        var homeButton = $("#home");
-
-        homeButton.on('click', function (){
-            homePage.empty();
-            contact.hide();
-            layout.hide();
-            leftSide.hide();
-            mainText.show();
-            portfolio.hide();
-            $(".graphic").css('font-weight', 'normal');
-            $("#home").css('font-weight', 'bold');
-            $("#portfolio").css('font-weight', 'normal');
-            $("#contact").css('font-weight', 'normal');
-        });
-    }
-
-    clickHome();
-
     function clickContact(){
         var contactButton = $("#contact");
 
         contactButton.on('click', function (){
             homePage.empty();
             mainText.hide();
-            contact.show();
             portfolio.hide();
+            contact.show();
             leftSide.hide();
+            form.hide();
             $(".graphic").css('font-weight', 'normal');
             $("#home").css('font-weight', 'normal');
             $("#contact").css('font-weight', 'bold');
@@ -90,105 +160,36 @@ $(document).ready(function(){
 
     clickContact();
 
-    function clickGraphicDesign(){
-        var graphicDesignBtn = $(".graphic");
+    function sendEmail(){
+        var emailButton = $("#emailButton");
 
-        graphicDesignBtn.on('click', function(){
-            graphicDesign.show();
-            layout.hide();
-            $(".graphic").css('font-weight', 'bold');
-            $(".web").css('font-weight', 'normal');
-            $(".brand").css('font-weight', 'normal');
-            $(".layout").css('font-weight', 'normal');
-            $(".free").css('font-weight', 'normal');
-        })
+        emailButton.on('click', function (){
+            homePage.empty();
+            mainPage.show();
+            form.show();
+            contact.hide();
+            $("#home").css('font-weight', 'normal');
+        });
     }
 
-    clickGraphicDesign();
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+      })
 
-    function clickWebDesign(){
-        var webDesignBtn = $(".web");
+    sendEmail();
 
-        webDesignBtn.on('click', function(){
-            portfolioHeader.show();
-            graphicDesign.hide();
-            // webDesign.show();
-            layout.hide();
-            $(".graphic").css('font-weight', 'normal');
-            $(".web").css('font-weight', 'bold');
-            $(".brand").css('font-weight', 'normal');
-            $(".layout").css('font-weight', 'normal');
-            $(".free").css('font-weight', 'normal');
-            
-        })
-    }
+    // function submitEmail(){
+    //     var submit = $("#submit");
 
-    clickWebDesign();
+    
+    // }
 
-    function clickBranding(){
-        var brandingBtn = $(".brand");
+    // submitEmail();
 
-        brandingBtn.on('click', function(){
-            portfolioHeader.show();
-            graphicDesign.hide();
-            // webDesign.hide();
-            // branding.show();
-            layout.hide();
-            $(".graphic").css('font-weight', 'normal');
-            $(".web").css('font-weight', 'normal');
-            $(".brand").css('font-weight', 'bold');
-            $(".layout").css('font-weight', 'normal');
-            $(".free").css('font-weight', 'normal');
-            
-        })
-    }
-
-    clickBranding();
-
-    function clickLayout(){
-        var layoutBtn = $(".layout");
-
-        layoutBtn.on('click', function(){
-            portfolioHeader.show();
-            graphicDesign.hide();
-            // webDesign.hide();
-            // branding.hide();
-            layout.show();
-            $(".graphic").css('font-weight', 'normal');
-            $(".web").css('font-weight', 'normal');
-            $(".brand").css('font-weight', 'normal');
-            $(".layout").css('font-weight', 'bold');
-            $(".free").css('font-weight', 'normal');
-            
-        })
-    }
-
-    clickLayout();
-
-    function clickFreelance(){
-        var freelanceBtn = $(".free");
-
-        freelanceBtn.on('click', function(){
-            portfolioHeader.show();
-            graphicDesign.hide();
-            // webDesign.hide();
-            // branding.hide();
-            layout.hide();
-            // freelance.show();
-            $(".graphic").css('font-weight', 'normal');
-            $(".web").css('font-weight', 'normal');
-            $(".brand").css('font-weight', 'normal');
-            $(".layout").css('font-weight', 'normal');
-            $(".free").css('font-weight', 'bold');
-            
-        })
-    }
-
-    clickFreelance();
 
     // Modal Images for Portfolio Page //
 
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('myModalImg');
 
     var img01 = document.getElementById('logic-01');
     var img02 = document.getElementById('logic-02');
